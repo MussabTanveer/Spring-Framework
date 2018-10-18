7cbee91590258f3dd80e628c4564c37b11c13d8f
Mussab Tanveer <mussab.tanveer9@gmail.com>
2018-10-18 14:37:42 +0500

    Dynamic auto wiring and Troubleshooting - @Primary

--- src/main/java/com/mussab/spring/basics/springin10steps/QuickSortAlgo.java
+++ src/main/java/com/mussab/spring/basics/springin10steps/QuickSortAlgo.java
@@ -1,5 +1,12 @@
 package com.mussab.spring.basics.springin10steps;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+//Bean
+@Component
+// Make this component primary
+@Primary
 public class QuickSortAlgo implements SortAlgo {
 	public int[] sort(int[] numbers){
 		// Logic for quick sort
